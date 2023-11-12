4781e55678169cba90dbd1d66cf7b2b748616524
Ana Beatriz Maciel Nunes <150164559+anabeatrizmaciel@users.noreply.github.com>
2023-11-12 18:30:23 -0400

    Update main_ana.py

--- main_ana.py
+++ main_ana.py
@@ -1,12 +1,17 @@
 import random
 import string
 
-frase = "METHINKS IT IS LIKE A WEASEL"
-populacao = 100
-chancedemutacao = 0.05
-geracao = 0 
+frase_alvo = "METHINKS IT IS LIKE A WEASEL"
+tamanho_populacao = 100
+chance_mutacao = 0.05
+geracao = 0
 
-string = ''.join(random.choice(string.ascii_uppercase + ' ') for _ in range(len(frase)))
-
-while frase != string
+frase_atual = ''.join(random.choice(string.ascii_uppercase + ' ') for _ in range(len(frase_alvo)))
     
+while frase_atual != frase_alvo:
+    populacao = [''.join(random.choice(string.ascii_uppercase + ' ') for _ in range(len(frase_alvo))) for _ in range(tamanho_populacao)]
+    aptidao = [(sum(1 for t, s in zip(frase_alvo, seq) if t == s), seq) for seq in populacao]
+    melhor_sequencia = max(aptidao, key=lambda x: x[0])
+    frase_atual = melhor_sequencia[1]
+    
+    if geracao % 100 == 0: 
