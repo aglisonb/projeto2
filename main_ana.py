bb57dc17b0f6252e1310fd345cb7273e34c8d29a
Ana Beatriz Maciel Nunes <150164559+anabeatrizmaciel@users.noreply.github.com>
2023-11-13 08:29:43 -0400

    Update main_ana.py

--- main_ana.py
+++ main_ana.py
@@ -1,17 +1,60 @@
 import random
-import string
 
 frase_alvo = "METHINKS IT IS LIKE A WEASEL"
+chance_mutacao = 5
 tamanho_populacao = 100
-chance_mutacao = 0.05
-geracao = 0
+char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
 
-frase_atual = ''.join(random.choice(string.ascii_uppercase + ' ') for _ in range(len(frase_alvo)))
+def sequencia_aleatoria(frase_alvo, char):
+    sequencia = ''
+    for i in range(len(frase_alvo)):
+        sequencia += char[random.randint(0, len(char)-1)]
+    return sequencia
+
+def pontuacao (sequencia, frase_alvo):
+    pontos = 0
+    for i in range(len(frase_alvo)):
+      if frase_alvo[i] == sequencia[i]:
+        pontos += 1
+    return pontos
+
+def mutacao(sequencia, char):
+    resultado_mutacao = ''
+    for i in range(len(sequencia)):
+      if random.randint(0, tamanho_populacao) <= chance_mutacao:
+        resultado_mutacao += char[random.randint(0, len(char)-1)]
+      else:
+        resultado_mutacao += sequencia[i]
+    return resultado_mutacao
+
+def sequencia_mutacoes(sequencia, char):
+    lista_seq = []
+    for i in range(100):
+      lista_seq.append(mutacao(sequencia, char))
+    return lista_seq
+
+def funcao_sequencia_modificada(sequencia, frase_alvo, char):
+  lista_seq = sequencia_mutacoes(sequencia, char)
+  melhor_sequencia = lista_seq[0]
+  melhor_fator = pontuacao(melhor_sequencia, frase_alvo)
+  for seq in lista_seq:
+    fator_gene = pontuacao(seq, frase_alvo)		
+    if fator_gene > melhor_fator:
+      melhor_fator = fator_gene
+      melhor_sequencia = seq
+  return melhor_sequencia
+
+
+def main():
+    geracoes = 0
+    sequencia_atual = sequencia_aleatoria(frase_alvo, char)
+    print("Geração:", geracoes)
+    print(sequencia_atual)
     
-while frase_atual != frase_alvo:
-    populacao = [''.join(random.choice(string.ascii_uppercase + ' ') for _ in range(len(frase_alvo))) for _ in range(tamanho_populacao)]
-    aptidao = [(sum(1 for t, s in zip(frase_alvo, seq) if t == s), seq) for seq in populacao]
-    melhor_sequencia = max(aptidao, key=lambda x: x[0])
-    frase_atual = melhor_sequencia[1]
-    
-    if geracao % 100 == 0: 
+    while sequencia_atual != frase_alvo:
+        sequencia_atual = funcao_sequencia_modificada(sequencia_atual, frase_alvo, char)
+        geracoes += 1
+        print("Geração:", geracoes)
+        print(sequencia_atual)
+
+main()
